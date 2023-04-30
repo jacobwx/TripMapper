3690936ea2af0a0f3afc6431e902391f4d694bb4
jacobwx <jacobwidan@gmail.com>
2023-04-30 15:55:39 -0500

    Cleaned up Driver class

--- src/Driver.java
+++ src/Driver.java
@@ -1,33 +1,9 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.swing.*;
-
-import org.openstreetmap.gui.jmapviewer.Coordinate;
-import org.openstreetmap.gui.jmapviewer.JMapViewer;
-import org.openstreetmap.gui.jmapviewer.tilesources.OsmTileSource;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.*;
 
 public class Driver {
-	
-	// Declare class data
-
     public static void main(String[] args) throws FileNotFoundException, IOException {
-
-    	// Read file and call stop detection
-    	TripPoint.readFile("triplog.csv");
-    	TripPoint.h2StopDetectionSimplified();
-    	
         MapGUI map = new MapGUI("triplog.csv");
-        
-        ArrayList<TripPoint> points = TripPoint.getMovingTrip();
     }
-    
-    // Animate the trip based on selections from the GUI components
     
 }
